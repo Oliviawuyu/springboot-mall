94924c2e7f7718d168988ed1a0d9d3b17f2c0c05
Olivia <s2993856@gmail.com>
2023-02-05 17:16:28 +0800

    get product list filtter by pages

--- src/main/java/com/oliviawu/springbootmall/controller/ProductController.java
+++ src/main/java/com/oliviawu/springbootmall/controller/ProductController.java
@@ -8,11 +8,15 @@ import com.oliviawu.springbootmall.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import java.util.List;
 
+@Validated
 @RestController
 public class ProductController {
 
@@ -26,13 +30,19 @@ public class ProductController {
             @RequestParam(required = false) String search,
             //排序
             @RequestParam(defaultValue = "created_date") String orderBy,
-            @RequestParam(defaultValue = "desc") String sort
+            @RequestParam(defaultValue = "desc") String sort,
+            //分頁
+            @RequestParam(defaultValue = "5") @Max(1000) @Min(0) Integer limit, //保戶資料庫 , 前端來得值只能介於0~1000
+            @RequestParam(defaultValue = "0") @Min(0) Integer offset //預設不跳過任何一筆數據
+                      //記得加@Validated到controller上，@Max(1000) @Min(0) 才會生效
     ){
         ProductQueryParams productQueryParams = new ProductQueryParams();
         productQueryParams.setCategory(category);
         productQueryParams.setSearch(search);
         productQueryParams.setOrderBy(orderBy);
         productQueryParams.setSort(sort);
+        productQueryParams.setLimit(limit);
+        productQueryParams.setOffset(offset);
 
         List<Product> productList = productService.getProducts(productQueryParams);
         return ResponseEntity.status(HttpStatus.OK).body(productList);//沒商品也要ＯＫ
