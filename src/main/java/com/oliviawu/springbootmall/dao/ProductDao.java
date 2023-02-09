8a41af3cf12f76923f6d6982ec3c92bb9ed9e758
Olivia <s2993856@gmail.com>
2023-02-09 19:00:25 +0800

    optimize create order stock

--- src/main/java/com/oliviawu/springbootmall/dao/ProductDao.java
+++ src/main/java/com/oliviawu/springbootmall/dao/ProductDao.java
@@ -18,7 +18,7 @@ public interface ProductDao {
     Integer createProduct(ProductRequest productRequest);
 
     void updateProduct(Integer productId, ProductRequest productRequest);
-
+    void updateStock(Integer productId,Integer stock);
     void deleteProductById(Integer productId);
 
 
--- src/main/java/com/oliviawu/springbootmall/service/impl/OrderServiceImpl.java
+++ src/main/java/com/oliviawu/springbootmall/service/impl/OrderServiceImpl.java
@@ -2,15 +2,21 @@ package com.oliviawu.springbootmall.service.impl;
 
 import com.oliviawu.springbootmall.dao.OrderDao;
 import com.oliviawu.springbootmall.dao.ProductDao;
+import com.oliviawu.springbootmall.dao.UserDao;
 import com.oliviawu.springbootmall.dto.BuyItem;
 import com.oliviawu.springbootmall.dto.CreateOrderRequest;
 import com.oliviawu.springbootmall.modal.Order;
 import com.oliviawu.springbootmall.modal.OrderItem;
 import com.oliviawu.springbootmall.modal.Product;
+import com.oliviawu.springbootmall.modal.User;
 import com.oliviawu.springbootmall.service.OrderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.beans.Transient;
 import java.util.ArrayList;
@@ -18,21 +24,45 @@ import java.util.List;
 
 @Component
 public class OrderServiceImpl implements OrderService {
+    private final static Logger log = LoggerFactory.getLogger(OrderServiceImpl.class);
 
     @Autowired
     private OrderDao orderDao;
     @Autowired
     private ProductDao productDao;
 
+    @Autowired
+    private UserDao userDao;
+
 
     @Transactional
     @Override
     public Integer createOrder(Integer userId, CreateOrderRequest createOrderRequest) {
+        //檢查User
+        User user = userDao.getUserById(userId);
+        if(user==null){
+            log.warn("The userId: {} not exist",userId);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
         int totalAmount=0;
         List<OrderItem> orderItemList=new ArrayList<>();
 
         for(BuyItem buyItem : createOrderRequest.getBuyItemList()){
             Product product = productDao.getProductById(buyItem.getProductId());
+
+            //檢查product是否存在，庫存是否足夠
+            if(product==null){
+                log.warn("The product {} not exist",buyItem.getProductId());
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            }else if(product.getStock()<buyItem.getQuantity()){
+                log.warn("The product {} is out of stock, remaining quantity is {}, quantity to buy is {}",buyItem.getProductId(),product.getStock(),buyItem.getQuantity());
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+
+            }
+            productDao.updateStock(product.getProductId(),product.getStock()- buyItem.getQuantity());
+
+
 
             //計算總價
             int amount = buyItem.getQuantity()*product.getPrice();
